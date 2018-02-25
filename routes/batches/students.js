d81b1f172bfacf6ccf03991f66ce1ee792897205
folkert89 <folkertburghout@hotmail.com>
2018-02-25 19:02:10 +0100

    post evaluation possible, next step create the conditions

--- routes/batches/students.js
+++ routes/batches/students.js
@@ -33,13 +33,50 @@ module.exports = io => {
         evaluations: []
       }
 
-      // if (req.batch.filter(student => student.name===req.body.name).length>0) {
-      //   const error = Error.new('You already joined this game!')
-      //   error.status = 401
-      //   return next(error)
-      // }
+      if (req.batch.students.filter(student => (student.name===req.body.name)).length>0) {
+        console.log('now')
+        const error = Error.new('You already joined this game!')
+        error.status = 401
+        return next(error)
+      }
 
       req.batch.students = req.batch.students.concat( newStudent )
+
+      req.batch.save()
+        .then((batch) => {
+          req.batch = batch
+          next()
+        })
+        .catch((error) => next(error))
+    },
+
+    (req, res, next) => {
+      io.emit('action', {
+        type: 'BATCH_STUDENTS_UPDATED',
+        payload: {
+          batch: req.batch,
+          students: req.batch.students
+        }
+      })
+      res.json(req.batch.students)
+    })
+
+    .post('/batches/:id/students/:studentId/evaluations', authenticate, loadBatch, (req, res, next) => {
+      if (!req.batch) { return next() }
+
+      const studentId = req.params.studentId
+
+      const student = req.batch.students.filter((s) => (s.id === studentId))[0]
+
+      const newEvaluation = {
+        evaluation: req.body.color,
+        date: req.body.date,
+        remark: req.body.remark,
+      }
+
+      console.log(newEvaluation)
+
+      student.evaluations = student.evaluations.concat( newEvaluation )
 
       req.batch.save()
         .then((batch) => {
